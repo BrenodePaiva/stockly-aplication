86fad3bc8514901d6c441353bd7411f94b8adce3
BrenodePaiva <brenopaiva552@gmail.com>
2025-09-19 16:41:37 -0300

    feat: add today revenue component with streaming

--- app/_data-access/dashboard/get-dashboard.ts
+++ app/_data-access/dashboard/get-dashboard.ts
@@ -18,7 +18,6 @@ export interface MostSoldProductDto {
 }
 
 interface DashboardDto {
-  todayRevenue: number;
   totalSales: number;
   totalStock: number;
   totalProducts: number;
@@ -56,13 +55,6 @@ export const getDashboard = async (): Promise<DashboardDto> => {
     });
   }
 
-  const todayRevenueQuery = `
-    SELECT SUM ("SaleProduct"."unitPrice" * "SaleProduct"."quantity") as "todayRevenue" 
-    FROM "SaleProduct"
-    JOIN "Sale" ON "SaleProduct"."saleId" = "Sale"."id"
-    WHERE "Sale"."date" >= $1 AND "Sale"."date" <= $2
-    `;
-
   const mostSoldProductsQuery = `
     SELECT "Product"."name", SUM ("SaleProduct"."quantity") as "totalSold", "Product"."price", "Product"."stock", "Product"."id" as "productId"
     FROM "SaleProduct"
@@ -71,15 +63,6 @@ export const getDashboard = async (): Promise<DashboardDto> => {
     ORDER BY "totalSold" DESC
     LIMIT 5
   `;
-
-  const startOfDay = new Date(new Date().setHours(0, 0, 0, 0));
-  const endOfDay = new Date(new Date().setHours(23, 59, 59, 999));
-
-  const todayRevenuePromise = db.$queryRawUnsafe<{ todayRevenue: number }[]>(
-    todayRevenueQuery,
-    startOfDay,
-    endOfDay,
-  );
 
   const totalSalesPromise = await db.sale.count();
 
@@ -101,22 +84,15 @@ export const getDashboard = async (): Promise<DashboardDto> => {
     }[]
   >(mostSoldProductsQuery);
 
-  const [
-    todayRevenue,
-    totalSales,
-    totalStock,
-    totalProducts,
-    mostSoldProducts,
-  ] = await Promise.all([
-    todayRevenuePromise,
-    totalSalesPromise,
-    totalStockPromise,
-    totalProductsPromise,
-    mostSoldProductsPromise,
-  ]);
+  const [totalSales, totalStock, totalProducts, mostSoldProducts] =
+    await Promise.all([
+      totalSalesPromise,
+      totalStockPromise,
+      totalProductsPromise,
+      mostSoldProductsPromise,
+    ]);
 
   return {
-    todayRevenue: todayRevenue[0].todayRevenue,
     totalSales,
     totalStock: Number(totalStock._sum.stock),
     totalProducts,
